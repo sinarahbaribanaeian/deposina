2e3c90b02c686b9c63a3724afdb79b63df1fd814
sinarahbaribanaeian <148692746+sinarahbaribanaeian@users.noreply.github.com>
2025-06-11 23:43:58 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,39 @@
+</div> <!-- container mt-4 kapanışı -->
+</body>
+<footer class="bg-dark text-white py-4 mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h5>Hakkımızda</h5>
+                <p>Kırtasiye Dükkanı olarak 2025'ten beri hizmetinizdeyiz. Kalite ve güvenin adresi!</p>
+            </div>
+            <div class="col-md-4">
+                <h5>Hızlı Linkler</h5>
+                <ul class="list-unstyled">
+                    <li><a href="index.php" class="text-white">Anasayfa</a></li>
+                    <li><a href="urunler.php" class="text-white">Ürünler</a></li>
+                    <li><a href="iletisim.php" class="text-white">İletişim</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h5>İletişim</h5>
+                <address>
+                    <p><i class="fas fa-map-marker-alt"></i> Bartın / Kırtepe Mahallesi</p>
+                    <p><i class="fas fa-phone"></i> 0537 977 4235</p>
+                    <p><i class="fas fa-envelope"></i> info@kirtasiyedukkani.com</p>
+                </address>
+            </div>
+        </div>
+        <hr />
+        <div class="text-center">
+            <p>&copy; 2025 Kırtasiye Dükkanı. Tüm Hakları Saklıdır.</p>
+        </div>
+    </div>
+</footer>
+
+<!-- Bootstrap & Script -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/scripts.js"></script>
+
+</body>
+</html>
